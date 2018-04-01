ecb7e8c586916cd7e3091bb67bc3cdfabdb90b5d
Richmond Brininger <richmondbrininger@gmail.com>
2018-04-01 14:44:19 -0400

    Commented out the info additions so it doesn't get duplicated on accident

--- src/main/java/recipebuilder/main.java
+++ src/main/java/recipebuilder/main.java
@@ -33,7 +33,7 @@ public class main {
 		Ingredient ig5 = new Ingredient(0, "salt", null);
 		Ingredient ig6 = new Ingredient(0, "tuna", null);
 		Ingredient ig7 = new Ingredient(0, "chicken", null);
-		
+		/*
 		sess.save(ig1);
 		sess.save(ig2);
 		sess.save(ig3);
@@ -41,22 +41,24 @@ public class main {
 		sess.save(ig5);
 		sess.save(ig6);
 		sess.save(ig7);
-		
+		*/
 		Set<Ingredient> ins1 = new HashSet<Ingredient>();
 		ins1.add(ig2);
 		ins1.add(ig3);
 		ins1.add(ig5);
 		Recipe re1 = new Recipe(0, "Pasta", "Bob", "Bobby", "Boil water, add sause, eat", null, ins1);
 		
-		sess.save(re1);
+		//sess.save(re1);
 		
 		Review rv1 = new Review(0,"Lee", "Brininger", 3, "Its ok I guess, needs more flavor", re1);
 		Review rv2 = new Review(0,"Will", "Jones", 3, "Its ok I guess, needs more flavor", re1);
 		Review rv3 = new Review(0,"Nick", "Peters", 3, "Its ok I guess, needs more flavor", re1);
 		
-		sess.save(rv1);
-		sess.save(rv2);
-		sess.save(rv3);
+		//sess.save(rv1);
+		//sess.save(rv2);
+		//sess.save(rv3);
+		
+		System.out.println(re1.getDescription());
 		
 		tx.commit();
 		
